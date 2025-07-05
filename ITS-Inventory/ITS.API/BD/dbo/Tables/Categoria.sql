4885570fc6b2026356df4908dae4c93421987a21
rangulo <rangulo@CR9-ITS-RANGULO>
2025-07-05 10:33:17 -0600

    Add new ITS-Inventory structure (WEB) renewed

--- ITS-Inventory/ITS.API/BD/dbo/Tables/Categoria.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Categoria] (
-    [Id]          INT            IDENTITY (1, 1) NOT NULL,
-    [Nombre]      NVARCHAR (100) NOT NULL,
-    [Descripcion] NVARCHAR (200) NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
